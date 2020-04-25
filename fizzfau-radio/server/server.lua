d801d881d64a2e65982f3da08f23eef702fc34c9
fizzfau <59974801+fizzfau@users.noreply.github.com>
2020-04-25 21:42:54 +0300

    Update server.lua

--- fizzfau-radio/server/server.lua
+++ fizzfau-radio/server/server.lua
@@ -2,6 +2,13 @@ ESX               = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+ESX.RegisterUsableItem('radio', function(source)
+
+	local xPlayer = ESX.GetPlayerFromId(source)
+	TriggerClientEvent('ls-radio:use', source)
+
+end)
+
 -- checking is player have item
 
 Citizen.CreateThread(function()
@@ -11,7 +18,7 @@ Citizen.CreateThread(function()
     for i=1, #xPlayers, 1 do
         local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
           if xPlayer ~= nil then
-              if xPlayer.getInventoryItem('radio').count == 1 then
+              if xPlayer.getInventoryItem('radio').count == 0 then
 
                 local source = xPlayers[i]
                 TriggerClientEvent('ls-radio:onRadioDrop', source)
